8d1065b7665f8605bf09941f85d3e320e734fb1e
gyan <gyan9427@gmail.com>
2021-07-25 22:07:18 +0530

    Generate monthly sales chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { Line } from 'react-chartjs-2';
+
+const data = {
+  labels: ['Jan', 'Mar', 'May', 'July', 'Oct'],
+  datasets: [
+    {
+      label: 'samsung sales',
+      data: [400, 1000, 4000, 800, 1500],
+      fill: true,
+      backgroundColor: 'rgba(255, 99, 132,0.5)',
+      borderColor: 'rgba(255, 99, 132, 0.2)',
+      pointBorderColor: '#2a8677',
+      pointBorderWidth:5,
+      tension:0.4,
+    },
+  ],
+};
+
+const options = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+};
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monthly sales</h1>
+      <div className="chart">
+         <Line data={data} options={options}/>
+      </div>
+      
     </div>
   );
 }
